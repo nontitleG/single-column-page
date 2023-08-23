c6f26e8d86521e28ff41ca4b56e00c756184eda8
OhYeah!! <beckbook2007@gmail.com>
2023-08-23 18:34:27 +0900

    [add] aboutページを追加
    sampleのabuotページを追

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+export const Head = () => (
+  <>
+    <title>about page</title>
+    <meta name="description" content="自己紹介" /></>
+)
+export default AboutPage
